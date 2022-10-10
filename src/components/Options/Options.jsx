55f5d82628071cf6c8895818b3cf310f5a6e690c
shihab0 <ataharshihab1@gmail.com>
2022-10-11 01:32:40 +0600

    styling every components

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -5,8 +5,8 @@ const Options = ({option, showCorrectOrWrong}) => {
 
     return (
         <div>
-            <div className='text-lg font-normal '>
-                <h2 onClick={() => showCorrectOrWrong(option)} className='border p-2 md:w-3/5 border-gray-700 m-2 cursor-pointer hover:bg-gray-200'>{option}</h2>
+            <div className='text-lg font-normal w-3/4 bg-emerald-100 m-auto'>
+                <h2 onClick={() => showCorrectOrWrong(option)} className='p-2 border-gray-700 m-2 cursor-pointer hover:bg-gray-200'>{option}</h2>
             </div>
         </div>
     );
--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import Options from '../Options/Options';
+import toast, { Toaster } from 'react-hot-toast';
+
+const correct = () => toast.success('Correct!! You Have Done.');
+const wrong = () => toast.error('Wrong!! Try Again.')
 
 const Question = ({questionData}) => {
     console.log(questionData);
-
+    
     const {id, options, question, correctAnswer} = questionData;
     console.log(id)
     const showCorrectOrWrong = (option) => {
         if(correctAnswer === option){
-            alert('correct')
+            {correct()}
         }else{
-            alert(`wrong, correct ans is ${correctAnswer}`)
+            {wrong()}
         }
     }
 
     return (
         <div>
-           <div className='w-3/4 m-auto'>
+           <div className='w-3/4 my-8 mx-auto bg-slate-400 p-2 rounded-lg'>
             <h4 className='text-xl font-semibold mt-7 text-left'>
               <li>{question}</li>
               {
@@ -24,6 +28,7 @@ const Question = ({questionData}) => {
               }
             </h4>
            </div>
+           <Toaster />
         </div>
     );
 };

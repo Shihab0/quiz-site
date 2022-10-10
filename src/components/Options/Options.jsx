ec6d5bf9af92ad595fcc8a10f89aa3c58b05a09b
shihab0 <ataharshihab1@gmail.com>
2022-10-11 01:00:14 +0600

    correct or not logic set

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 
-const Options = ({option}) => {
-    
-    const showCorrectOrWrong = () => {
-        alert('hi')
-    }
+const Options = ({option, showCorrectOrWrong}) => {
+
 
     return (
         <div>
             <div className='text-lg font-normal '>
-                <h2 onClick={() => showCorrectOrWrong()} className='border p-2 md:w-3/5 border-gray-700 m-2 cursor-pointer hover:bg-gray-200'>{option}</h2>
+                <h2 onClick={() => showCorrectOrWrong(option)} className='border p-2 md:w-3/5 border-gray-700 m-2 cursor-pointer hover:bg-gray-200'>{option}</h2>
             </div>
         </div>
     );
--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -2,15 +2,25 @@ import React from 'react';
 import Options from '../Options/Options';
 
 const Question = ({questionData}) => {
-    // console.log(questionData);
-    const {options, question, correctAnswer} = questionData;
+    console.log(questionData);
+
+    const {id, options, question, correctAnswer} = questionData;
+    console.log(id)
+    const showCorrectOrWrong = (option) => {
+        if(correctAnswer === option){
+            alert('correct')
+        }else{
+            alert(`wrong, correct ans is ${correctAnswer}`)
+        }
+    }
+
     return (
         <div>
            <div className='w-3/4 m-auto'>
             <h4 className='text-xl font-semibold mt-7 text-left'>
               <li>{question}</li>
               {
-                options.map(option => <Options option={option}></Options> )
+                options.map(option => <Options showCorrectOrWrong={showCorrectOrWrong} option={option}></Options> )
               }
             </h4>
            </div>

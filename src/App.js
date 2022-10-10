a10dac139b0495d59481b4d55e7ca9ae17c7603a
shihab0 <ataharshihab1@gmail.com>
2022-10-10 21:30:28 +0600

    router and navbar added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layout/Main/Main';
+import Home from './components/Home/Home';
+import Topics from './components/Topics/Topics';
+import Blogs from './components/Blogs/Blogs';
+import Statistics from './components/Statistics/Statistics';
 
 function App() {
 
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <div> Hello, World</div>
-    }
-
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>,  
+        },
+        {
+          path: '/topics',
+          element: <Topics></Topics>
+        },
+        {
+          path: '/statistics',
+          element: <Statistics></Statistics>
+        },
+        {
+          path: '/blogs',
+          element: <Blogs></Blogs>
+        }
+      ]
+    }, 
   ])
 
   return (
---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <div className=''>
+            <div className='flex justify-between bg-slate-700 text-white text-lg py-4 px-6 font-semibold'>
+               <div>
+                  <h4>Test YourSelf</h4>
+               </div>
+               <div className='flex gap-4'>
+                  <Link to='topics'>Topics</Link>
+                  <Link to='statistics'>Statistics</Link>
+                  <Link to='blogs'>Blogs</Link>
+               </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
